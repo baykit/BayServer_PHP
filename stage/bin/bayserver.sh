d556c6d9355dbe9e809ac376729de6e58da5bc28
MichisukeP <michisukep@gmail.com>
2022-11-30 23:15:22 +0900

    Modifies startup script for test command of /bin/sh

--- stage/bin/bayserver.sh
+++ stage/bin/bayserver.sh
@@ -4,13 +4,13 @@ base=`dirname $0`
 args=$*
 daemon=
 for arg in $args; do
-  if [ "$arg" == "-daemon" ]; then
+  if [ "$arg" = "-daemon" ]; then
     daemon=1
   fi
 done
 
 cmd=php
-if [ "$daemon" == 1 ]; then
+if [ "$daemon" = 1 ]; then
    $cmd $base/bootstrap.php $* < /dev/null  > /dev/null 2>&1 &
 else
    $cmd $base/bootstrap.php $* 
