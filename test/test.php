439cbef4f99fd37dbf6c3957fee26cd4b503060b
Michisuke-P <michisukep@gmail.com>
2023-11-14 17:16:45 +0900

    Modifies the test script

--- test/test.php
+++ test/test.php
@@ -1,19 +1,24 @@
 <?php
 
+require 'vendor/autoload.php';
 
 $bhome = dirname(__FILE__) . "/..";
 
 $sla = "/";
 $path = get_include_path();
-$path = $path . PATH_SEPARATOR . $bhome . $sla . "core";
 
-$dockers = "$bhome" . $sla . "docker";
+$dockers = "$bhome" . $sla . "packages";
 $dirs = glob("$dockers/*", GLOB_ONLYDIR);
 foreach ($dirs as $dir) {
     $path = $path . PATH_SEPARATOR . $dir;
 }
 
+print  $path . PHP_EOL;
 ini_set('include_path', $path);
+
+$blib = "../../packages/bayserver";
+putenv("BSERV_LIB={$blib}");
+
 
 require 'baykit/bayserver/BayServer.php';
 
